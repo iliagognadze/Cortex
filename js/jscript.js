4cef1cb817e401610cb7b549dc507af2f01fc58c
iliagognadze <50463093+iliagognadze@users.noreply.github.com>
2019-06-04 22:50:59 +0400

    Add files via upload

---
+++ js/jscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	// $('.nav-item').on('mouseover', function(){
+	// 	$('#linkUnder').remove();
+	// });
+
+	$('#submit').on('click', function(e){
+
+ 			$('#area').val('');
+ 			$('#area1').val('');
+ 			$('#area2').val('');
+ 			$('#area3').val('');
+
+ 			$('#thank').addClass('d-block');
+ 		});
+
+});
